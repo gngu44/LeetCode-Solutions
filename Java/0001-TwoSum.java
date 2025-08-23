afd92deebc6caf48780d889ce57907e1dfafae32
gngu44 <197215360+gngu44@users.noreply.github.com>
2025-08-23 14:11:30 -0400

    TwoSum

---
+++ Java/0001-TwoSum.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> pairs = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++){
+            int num = nums[i];
+            if (pairs.containsKey(target - num)){
+                return new int[] {i, pairs.get(target-num)};
+            }
+            pairs.put(num, i);
+
+        }
+
+        return new int[] {};
+    }
+}
